7c41014fdbe15941a45a85416446f0be170fe021
Tunde <tundeogunmodede1@gmail.com>
2024-06-13 04:18:05 -0700

    second task

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,7 +2,7 @@
 # holberton user open without any error message
 
 
-exec { 'OS security config'
+exec {'OS security config':
   command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
-  path    => '/usr/bin/env:/bin:/usr/bin:/usr/sbin/',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/',
 }
