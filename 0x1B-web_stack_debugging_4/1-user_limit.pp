56d2eee3357d779275a7c2a9e762aeac4c91160b
Tunde <tundeogunmodede1@gmail.com>
2024-06-13 04:16:21 -0700

    second task

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -4,5 +4,5 @@
 
 exec { 'OS security config'
   command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
-  path    => '/usr/bin/env/:bin/:/usr/bun/:usr/sbin/',
+  path    => '/usr/bin/env:/bin:/usr/bin:/usr/sbin/',
 }
